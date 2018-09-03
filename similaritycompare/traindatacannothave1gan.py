dd845a358078f8bada07cd478abd1c9b18783553
Ubuntu <ubuntu@hpcc.novalocal>
2018-09-03 15:04:19 +0000

    3wganepoch

--- similaritycompare/traindatacannothave1gan.py
+++ similaritycompare/traindatacannothave1gan.py
@@ -9,9 +9,9 @@ tf.set_random_seed(1)
 np.random.seed(1)
 
 # Hyper Parameters
-BATCH_SIZE = 100
-LR_G = 0.0001           # learning rate for generator
-LR_D = 0.0001           # learning rate for discriminator
+BATCH_SIZE = 200 
+LR_G = 0.0001    # learning rate for generator
+LR_D = 0.0001    # learning rate for discriminator
 N_IDEAS = 10             # think of this as number of ideas for generating an art work (Generator)
 number_of_useorders = 4*5  #number of high similarity used * (order of similarity + 1)
 
@@ -87,33 +87,36 @@ def artist_works():     # painting from the famous artist (real target)
 
 with tf.variable_scope('Generator'):
     G_in = tf.placeholder(tf.float32, [None, N_IDEAS])          # random ideas (could from normal distribution)
-    G_l1 = tf.layers.dense(G_in, 64, tf.nn.relu)
+    G_l1 = tf.layers.dense(G_in, 32, tf.nn.relu)
     G_out = tf.layers.dense(G_l1, number_of_useorders, tf.nn.sigmoid)               # making a orderlist from these random ideas
 
 with tf.variable_scope('Discriminator'):
     real_order = tf.placeholder(tf.float32, [None, number_of_useorders], name='real_in')   # receive order from the real data
-    D_l0 = tf.layers.dense(real_order, 128, tf.nn.relu, name='l')
-    prob_orderreal0 = tf.layers.dense(D_l0, 1, tf.nn.sigmoid, name='out')              # probability that the order is real
+    D_l0 = tf.layers.dense(real_order, 32, tf.nn.relu, name='l')
+    prob_orderreal0 = tf.layers.dense(D_l0, 1, name='out')              # probability that the order is real
     # reuse layers for generator
-    D_l1 = tf.layers.dense(G_out, 128, tf.nn.relu, name='l', reuse=True)            # receive art work from a newbie like G
-    prob_orderreal1 = tf.layers.dense(D_l1, 1, tf.nn.sigmoid, name='out', reuse=True)  # probability that the order is real
+    D_l1 = tf.layers.dense(G_out, 32, tf.nn.relu, name='l', reuse=True)            # receive art work from a newbie like G
+    prob_orderreal1 = tf.layers.dense(D_l1, 1, name='out', reuse=True)  # probability that the order is real
+theta_D = [D_l0, prob_orderreal0, D_l1, prob_orderreal1]
+D_loss = -tf.reduce_mean(prob_orderreal0) + tf.reduce_mean(prob_orderreal1)
+G_loss = tf.reduce_mean(-prob_orderreal1)
 
-D_loss = -tf.reduce_mean(tf.log(prob_orderreal0) + tf.log(1-prob_orderreal1))
-G_loss = tf.reduce_mean(tf.log(1-prob_orderreal1))
-
-train_D = tf.train.AdamOptimizer(LR_D).minimize(
+train_D = tf.train.RMSPropOptimizer(LR_D,0.9).minimize(
     D_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Discriminator'))
-train_G = tf.train.AdamOptimizer(LR_G).minimize(
+train_G = tf.train.RMSPropOptimizer(LR_G,0.9).minimize(
     G_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Generator'))
+prob_orderreal0 = tf.clip_by_value(prob_orderreal0, 0.0, 1.0)
+prob_orderreal1 = tf.clip_by_value(prob_orderreal1, 0.0, 1.0)
 
-
+saver = tf.train.Saver()
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-
+#saver.restore(sess, 'wgantmp/model.ckpt')
 print("train start")
+#i = 44*BATCH_SIZE 
 i = 0
 for step in range(50000):
-    print("epoch " + str(step))
+    print("epoch " + str(step+0))
     print("start from " + str(i))
     if (i > (lendata_shuffle - 3)):
         i = 0
@@ -123,10 +126,13 @@ for step in range(50000):
     batch_real = []
     for pair in dataepoch:
         batch_real.append(similaritybetweeneachsourcewheresinkexist_comparetosource_fortrain(pair[0], pair[1], data_shuffle, sourcedata))
-    sess.run([train_D, train_G], {G_in: G_ideas, real_order: batch_real})
+    print("epoch start train")
+    for iter in range(40):
+        sess.run([train_D, train_G, prob_orderreal0, prob_orderreal1], {G_in: G_ideas, real_order: batch_real})
     print("test start")
     prediction_test = sess.run(prob_orderreal0, {real_order: testsimlist})
-    csvname = str(step) + 'ganmethodcancel1intrain.csv'
+    csvname = str(step+0) + 'ganmethodcancel1intrain.csv'
+    save_path = saver.save(sess, 'wgantmp/model.ckpt')
     with open(csvname, 'wb') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         filewriter.writerow(['Id', 'Prediction'])
